2c06745841b5f01fce640ccb829386b613708683
Kulbir <kulbir2408@gmail.com>
2018-10-13 21:25:47 +0530

    Adding the DAO Layer and updating readme

---
+++ src/com/model/DailyCandle.java
@@ -0,0 +1,47 @@
+package com.model;
+
+public class DailyCandle {
+	
+	public String time;
+	public String open;
+	public String high;
+	public String low;
+	public String close;
+	public String volume;
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getOpen() {
+		return open;
+	}
+	public void setOpen(String open) {
+		this.open = open;
+	}
+	public String getHigh() {
+		return high;
+	}
+	public void setHigh(String high) {
+		this.high = high;
+	}
+	public String getLow() {
+		return low;
+	}
+	public void setLow(String low) {
+		this.low = low;
+	}
+	public String getClose() {
+		return close;
+	}
+	public void setClose(String close) {
+		this.close = close;
+	}
+	public String getVolume() {
+		return volume;
+	}
+	public void setVolume(String volume) {
+		this.volume = volume;
+	}
+}
