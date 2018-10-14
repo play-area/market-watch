b60e72bb3fe33ebb5c10d74ceb3606ea4b074fd9
ravsingh7 <35032051+ravsingh7@users.noreply.github.com>
2018-10-14 13:23:31 +0530

    Adding Property File changes

---
+++ src/com/quandl/connection/ReadConfig.java
@@ -0,0 +1,16 @@
+package com.quandl.connection;
+import java.io.IOException;
+
+public class ReadConfig {
+	
+	public static void main(String[] args) throws IOException {
+		ConfigPropertyValues properties = new ConfigPropertyValues();
+		properties.getPropValues();
+	}
+
+}
+
+
+
+
+ 
