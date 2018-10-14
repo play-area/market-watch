dc770b9fd98bffb56399fb8561616f2299deaa0b
ravsingh7 <35032051+ravsingh7@users.noreply.github.com>
2018-10-14 18:22:17 +0530

    Changes

--- src/com/quandl/connection/QuandlConnection.java
+++ src/com/quandl/connection/QuandlConnection.java
@@ -1,21 +1,40 @@
 package com.quandl.connection;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import com.model.DailyCandleDTO;
+import com.model.QuandlResponseDTO;
+import com.util.DailyCandleJSONParser;
+import com.util.DataUtil;
 
 public class QuandlConnection {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		HttpClient client = new DefaultHttpClient();
 		//api URL
-		HttpGet request = new HttpGet("https://www.quandl.com/api/v3/datasets/NSE/VEDL.json?api_key=UXsVDnMsYhtX_qVmVVLR");
+		ConfigPropertyValues configProperty = new ConfigPropertyValues();
+		String url = configProperty.url;
+		HttpGet request = new HttpGet(url);
 		HttpResponse response;
 		try {
 			response = client.execute(request);
+			
+			DailyCandleJSONParser jsonParser = new DailyCandleJSONParser();
+			
+			
+			
+			QuandlResponseDTO data = jsonParser.fromJSON(EntityUtils.toString(response.getEntity()));
+			
+			List<DailyCandleDTO> dailyCandleData = DataUtil.getDailyCandleDataList(data);
 		
 			// Get the response
 			BufferedReader br;
